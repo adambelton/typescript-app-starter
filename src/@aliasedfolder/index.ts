3240c0ce1bfea5aeefe404433bb63a1ad15702ea
Adam Belton <adambelton@outlook.com>
2020-07-05 20:48:58 +0100

    add note about aliasing folders

--- src/@aliasedfolder/index.ts
+++ src/@aliasedfolder/index.ts
@@ -1,1 +1,1 @@
-// This is an alias folder. Aliasing allows you to use absolute imports.
+// This is an alias folder. Aliasing allows you to use absolute imports. Add more aliases in webpack.config.js.
